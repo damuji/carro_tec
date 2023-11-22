704e5eda47405dafe39bfb24c1d16f8a27558cd8
daniel muñoz <damuji2001@gmail.com>
2023-11-22 14:26:48 -0600

    ajustes finos

--- reciver/Core/Src/apps/pid1/pid1.c
+++ reciver/Core/Src/apps/pid1/pid1.c
@@ -9,21 +9,24 @@
 #include "../variables/variables.h"
 
 extern TIM_HandleTypeDef htim1;
-int kp=42;
-int ki=1;
+int kp=65;
+int ki=5;
 int kd=0;
 int integral =0;
 int deribativa =0;
 int proporcional=0;
-int objetivo =1850;
+int objetivo =1300;
 int milis = 25;
 int total =0;
+int antes=0;
 void mover();
 void pid1(){
 	int error =0;
 	error = objetivo - getVolante();
 	proporcional = (error * kp)/1000;
 	integral = (error*25)+integral;
+	diferencial = (antes-error)/25;
+	antes = error;
 	total =(integral*ki)/100000    +  proporcional;
 	mover();
 }
@@ -44,7 +47,7 @@ void mover(){
 		HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
 	}
 	else{
-		TIM1->CCR1 = 0;
+		TIM1->CCR1 =0;
 		TIM1->CCR2 =0;
 	}
 }
